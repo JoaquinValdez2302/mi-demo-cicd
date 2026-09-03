e46ce0deae7ca2204c772624cb595e2a770e04aa
Cocacolagua <lucasrabinovich181004@gmail.com>
2026-09-03 11:26:39 -0300

    arreglar error 1

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ app.get('/', (req, res) => {
 
 module.exports = app;
 
-error 1
-
 if (require.main === module) {
     app.listen(PORT, () => {
         console.log(`Servidor corriendo en puerto ${PORT}`);
